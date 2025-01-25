bc2a34a31e922d53cf2db74a661f60cdad47bb04
Joaquin De Losada <montainproductions@gmail.com>
2025-01-25 11:54:33 -0500

    Added max health

--- GlobalGamJam2025/Assets/Scripts/Sc_Health.cs
+++ GlobalGamJam2025/Assets/Scripts/Sc_Health.cs
@@ -3,15 +3,24 @@ using UnityEngine;
 public class Sc_Health : MonoBehaviour
 {
     [SerializeField]
-    private float health;
+    private float maxHealth;
+
+    private float currentHealth;
 
     [SerializeField]
     private GameObject gameObject;
 
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
     public void UpdateHealth(float newHealth){
+        if(currentHealth + newHealth > maxHealth) {currentHealth = maxHealth; return;}
+        
         health += newHealth;
 
-        if(health <= 1){
+        if(health < 1){
             health = 0;
             Destroy(gameObject);
         }
