2984362a758a01f7e614a003459ddc9c05ae17f2
Joaquin De Losada <montainproductions@gmail.com>
2025-01-24 22:33:36 -0500

    Script GameManager

---
+++ GlobalGamJam2025/Assets/Scripts/Sc_GameManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sc_GameManager : MonoBehaviour
+{
+    private int currentLevel=0, enemiesToSpawn;
+
+    [SerializeField]
+    private GameObject baseEnemy;
+
+    [SerializeField]
+    private GameObject[] spawnPoint1, spawnPoint2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
